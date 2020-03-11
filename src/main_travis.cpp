b8c3c3b459d806db8c4f30abc813eea04eea02b4
pershik <k.pershik@gmail.com>
2020-03-11 23:24:45 +0100

    Added CMakeLists and code example. It's intentionally not buildable.

---
+++ src/main_travis.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "travis/travis.h"
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    std::ciut << sum(a, b) << std::endl;
+}
---
+++ src/travis/travis.cpp
@@ -0,0 +1,5 @@
+#include "travis/travis.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
