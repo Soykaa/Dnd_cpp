8f395af1f92aeef16a40b79b290beeb9d9f38658
Jane Kirillova <grindevald666@gmail.com>
2020-05-28 23:21:20 +0300

    Fix mistake

--- DnD/src/chargentree.cpp
+++ DnD/src/chargentree.cpp
@@ -83,7 +83,7 @@ void CharGenTree::on_next_step_clicked() {
     Backpack bp;
     game->boards[0].heroes[0].h = new Hero(cs, bp);
     t1.generateChar(game->boards[0].heroes[0].h, cur_node);
-    game->boards[1].heroes[0].h = game->boards[0].heroes[1].h;
+    game->boards[1].heroes[0].h = game->boards[0].heroes[0].h;
     int num;
     if (game->boards[0].heroes[0].h->getRace() == CharRace::dragonborn) {
         num = rand() % 3;
